909fc7e20a3494f925def6f5c707456ce2ed922c
anukul rai <anukul.rau@gmail.com>
2025-07-15 00:39:55 +0545

    more updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import { Route, Routes, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import PredictDisease from "./pages/PredictDisease";
+import Appointments from "./pages/Appointments";
+import Articles from "./pages/Articles";
+import Profile from "./pages/Profile";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useAuth, AuthProvider } from "./context/AuthContext";
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const { loggedIn } = useAuth();
+
+    return (
+        <>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route
+            path="/login"
+            element={loggedIn ? <Navigate to="/" /> : <Login />}
+            />
+            <Route
+            path="/signup"
+            element={loggedIn ? <Navigate to="/" /> : <Signup />}
+            />
+            <Route
+            path="/predict"
+            element={loggedIn ? <PredictDisease /> : <Navigate to="/login" />}
+            />
+            <Route
+            path="/book"
+            element={loggedIn ? <Appointments /> : <Navigate to="/login" />}
+            />
+            <Route
+            path="/article"
+            element={loggedIn ? <Articles /> : <Navigate to="/login" />}
+            />
+            <Route
+            path="/profile"
+            element={loggedIn ?  <Navigate to="/login" /> : <Profile />}
+            />
+        </Routes>
+        <ToastContainer />
+        </>
+    );
 }
 
-export default App
+export default function AppWrapper() {
+    return (
+        <AuthProvider>
+        <App />
+        </AuthProvider>
+    );
+}
